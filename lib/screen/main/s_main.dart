735b9932c9072ac7d95b22134256f6576deb8f2a
Astrum93 <jungmin2670@gmail.com>
2024-01-21 21:08:41 +0900

    240121 Live Background 적용 완료

--- lib/screen/main/s_main.dart
+++ lib/screen/main/s_main.dart
@@ -5,6 +5,9 @@ import 'package:fast_app_base/common/cli_common.dart';
 import 'package:fast_app_base/common/widget/animated_number_text.dart';
 import 'package:fast_app_base/common/widget/line_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:live_background/object/palette.dart';
+import 'package:live_background/object/particle_shape_type.dart';
+import 'package:live_background/widget/live_background_widget.dart';
 import 'package:web_socket_channel/io.dart';
 
 import '../../common/common.dart';
@@ -54,21 +57,38 @@ class MainScreenState extends State<MainScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: const MenuDrawer(),
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              AnimatedNumberText(
-                priceString,
-                textStyle:
-                    const TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
-                duration: 50.ms,
-              ),
-              LineChartWidget(priceList, maxPrice: maxPrice)
-            ],
+      body: Stack(
+        children: [
+          const LiveBackgroundWidget(
+            shape: ParticleShapeType.square,
+            velocityY: -7,
+            particleMinSize: 5,
+            particleMaxSize: 25,
+            particleCount: 3000,
+            palette: Palette(
+              colors: [
+                Color(0xff165B33),
+                Color(0xff83ec00),
+              ],
+            ),
           ),
-        ),
+          SafeArea(
+            child: Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  AnimatedNumberText(
+                    priceString,
+                    textStyle: const TextStyle(
+                        fontSize: 50, fontWeight: FontWeight.bold),
+                    duration: 50.ms,
+                  ),
+                  LineChartWidget(priceList, maxPrice: maxPrice)
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
